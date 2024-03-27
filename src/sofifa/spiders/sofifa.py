1d1697d3aa5a905c2a32ca5dadfff1f4d8a4e136
Martin Klöckner <github@mkloeckner.net>
2024-03-27 20:51:26 +0100

    fix data selectors, globalize USER_AGENT, allow scrapibg of specific roster

--- src/sofifa/spiders/sofifa.py
+++ src/sofifa/spiders/sofifa.py
@@ -1,5 +1,6 @@
 import scrapy
 from .utils import *
+from scrapy.utils.project import get_project_settings
 import requests
 
 
@@ -9,55 +10,55 @@ class SofifaSpider(scrapy.Spider):
 
     teams_cache = dict()
 
-    def __init__(self, year="14", remap_columns="True"):
-        self.year = year
+    def __init__(self, fifa_version="140002", remap_columns="True"):
+        self.fifa_version = fifa_version
         self.remap_columns = remap_columns.lower() in ("true", "yes", "y", "1")
         self.start_urls = [self.__build_request_url()]
-        self.logger.info(f"Scraping year {YEAR_KEYS[year]}")
+        self.logger.info(f"Scraping version {fifa_version}")
 
     def __build_request_url(self) -> str:
-        return f"{PLAYERS_BASE_URL}&r={YEAR_KEYS[self.year]}"
+        return f"{PLAYERS_BASE_URL}&r={self.fifa_version}&set=true"
 
     def start_requests(self):
         request = scrapy.Request(self.start_urls[0])
-        request.cookies["r"] = YEAR_KEYS[self.year]
-        request.headers[
-            "User-Agent"
-        ] = "Mozilla/5.0 (X11; Linux x86_64; rv:100.0) Gecko/20100101 Firefox/100.0"
+        request.cookies["r"] = self.fifa_version
         yield request
 
     def parse(self, response):
-        props_headers = response.css("table thead tr th ::text").extract()[7:]
+        props_headers = response.css("table thead tr th ::text").extract()[6:]
         props_headers = list(map(clean_string, [_ for _ in props_headers]))
 
         if self.remap_columns:
             props_headers = rename_columns(props_headers)
 
-        for player in response.css("table.table > tbody > tr"):
-            club_name = player.css("td:nth-child(6) div.ellipsis a::text").get()
-            team_url = player.css("td:nth-child(6) div.ellipsis a::attr(href)").get()
+        for player in response.css("table > tbody > tr"):
+            club_name = player.css("td:nth-child(6) a::text").get()
+            team_url = player.css("td:nth-child(6) a::attr(href)").get()
 
             if club_name is None or str.isdecimal(club_name):
                 # skip fake players or players that do not play in any club
+                self.logger.info(f"Skipping fake player.")
                 continue
 
             item = {
-                "sofifa_id": player.css("td.col-pi::text").get(),
+                "sofifa_id": player.css("td:nth-child(7)::text").get(),
+                "fifa_version": self.fifa_version,
                 "player_url": player.css("td:nth-child(2) a::attr(href)").get(),
-                "short_name": player.css("td:nth-child(2) a div.ellipsis::text").get(),
-                "age": player.css("td.col-ae::text").get(),
+                "name": player.css("td:nth-child(2) a::attr(data-tippy-content)").get(),
+                "age": player.css("td:nth-child(3)::text").get(),
                 "nationality": player.css(
                     "td:nth-child(2) img.flag::attr(title)"
                 ).get(),
-                "club_name": player.css("td:nth-child(6) div.ellipsis a::text").get(),
+                "club_name": player.css("td:nth-child(6) a::text").get(),
                 "league_name": self.parse_team(team_url),
-                "player_positions": [player.css("td.col-bp a span::text").get()],
-                "potential": player.css("td.col-pt span::text").get()
+                "player_positions": [player.css("td:nth-child(2) span::text").get()],
+                "potential": player.css("td:nth-child(5) em::text").get(),
+                "overall_rating": player.css("td:nth-child(4) em::text").get()
             }
 
             props_values = []
 
-            for p in player.css("td")[8:]:
+            for p in player.css("td")[7:]:
                 value = p.css(" ::text").get()
                 if value is None:
                     value = ""
@@ -74,13 +75,14 @@ class SofifaSpider(scrapy.Spider):
         if team_url in self.teams_cache.keys():
             return self.teams_cache[team_url]
 
-        cookies = {"r": YEAR_KEYS[self.year]}
+        cookies = {"r": self.fifa_version}
+        settings = get_project_settings()
         headers = {
-            "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:100.0) Gecko/20100101 Firefox/100.0"
+            "User-Agent": settings.get('USER_AGENT')
         }
 
         req = requests.get(
-            SITE_BASE_URL + team_url + "?r=" + YEAR_KEYS[self.year],
+            SITE_BASE_URL + team_url + "?r=" + self.fifa_version,
             cookies=cookies,
             headers=headers,
         )
@@ -88,7 +90,7 @@ class SofifaSpider(scrapy.Spider):
         resp = scrapy.Selector(req)
 
         # the item is something like `English Premier League (1)`
-        league_name = resp.css(".info a::text").get()[:-4]
-        self.teams_cache[team_url] = league_name
+        league_name = resp.css("div.profile.clearfix a::text").get()
 
+        self.teams_cache[team_url] = league_name
         return league_name
