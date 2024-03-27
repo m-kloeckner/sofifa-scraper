4f548c1fb00a88973072ca9bf97b848479b15833
Martin Klöckner <github@mkloeckner.net>
2024-03-27 20:48:10 +0100

    add AntiBanMiddleware (via cloudscraper)

--- src/sofifa/middlewares.py
+++ src/sofifa/middlewares.py
@@ -4,6 +4,8 @@
 # https://docs.scrapy.org/en/latest/topics/spider-middleware.html
 
 from scrapy import signals
+from scrapy.http import HtmlResponse
+import cloudscraper
 
 # useful for handling different item types with a single interface
 from itemadapter import is_item, ItemAdapter
@@ -101,3 +103,18 @@ class SofifaDownloaderMiddleware:
 
     def spider_opened(self, spider):
         spider.logger.info('Spider opened: %s' % spider.name)
+
+class AntiBanMiddleware:
+    cloudflare_scraper = cloudscraper.create_scraper()
+
+    def process_response(self, request, response, spider):
+        request_url = request.url
+        response_status = response.status
+        if response_status not in (403, 503):
+            return response
+
+        spider.logger.info("Cloudflare detected. Using cloudscraper on URL: %s", request_url)
+        cflare_response = self.cloudflare_scraper.get(request_url)
+        cflare_res_transformed = HtmlResponse(url=request_url, body=cflare_response.text, encoding='utf-8')
+        #cflare_res_transformed = cflare_response.text
+        return cflare_res_transformed
